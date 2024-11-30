1e3aa2cfe84c48f84d908691bd38be30b189225c
prayagrmehta <prayagrmehta.1011@gmail.com>
2024-11-30 16:44:31 +0530

    static solve

--- prayag_portfolio/settings.py
+++ prayag_portfolio/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'prayag_portfolio.urls'
