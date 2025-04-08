d878f17b1976e39a9642d529a23652abec9984aa
prayagrmehta <prayagrmehta.1011@gmail.com>
2025-04-08 23:05:38 +0530

    sta commit

--- prayag_portfolio/settings.py
+++ prayag_portfolio/settings.py
@@ -124,6 +124,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
